fcce4bbb7b233e6ad4fe75b44afb6cb30b204466
wyf2020 <accwyf@gmail.com>
2022-06-26 22:32:57 +0800

    finish fine model

--- nerf.py
+++ nerf.py
@@ -1,6 +1,7 @@
 from ast import arg
 from email.policy import default
 from operator import truediv
+from traceback import print_stack
 import torch
 import configargparse
 import imageio
@@ -97,7 +98,7 @@ def poses2rays(render_poses, args):
 
     hi, wi = torch.meshgrid(torch.linspace(0,H-1,H), torch.linspace(0,W-1,W), indexing = 'ij')
     d = torch.stack([(wi-W//2)/f, -(hi-H//2)/f, -torch.ones(wi.shape)], axis = -1)
-    '''z轴需要加负号,这是因为相机面朝-z; 但是y轴加负号只是改变了光线的顺序, 这里与原版保持一致'''
+    '''z轴需要加负号, 这是因为相机面朝-z; 但是y轴加负号只是改变了光线的顺序, 这里与原版保持一致'''
     # version 1 比cpu的numpy版本快4倍
     # new_d = render_poses[:,None,:,:3] @ d.reshape(-1,3,1) # N,H*W,3,1
     # version 2 在gpu上300-600倍快于version1
@@ -139,11 +140,11 @@ def rays2points(rays, near, far, bds, args):
     else:
         pass
     
-    return points
+    return points, p
 
-def raws2pixels(raws, points, args):
-    raws = raws.reshape(-1, args.N_samples, 4)
-    points = points.reshape(-1, args.N_samples, 3+3)
+def raws2pixels(raws, points, args, samples):
+    raws = raws.reshape(-1, samples, 4)
+    points = points.reshape(-1, samples, 3+3)
     x,d = points[:,:,:3], points[:,0,3:6] # x[N*H*W,N_samples,3], d[N*H*W,3]
     rgb, sigma = raws[:,:,:3], raws[:,:,3] # rgb[N*H*W,N_samples,3], sigma[N*H*W,N_samples]
 
@@ -154,10 +155,11 @@ def raws2pixels(raws, points, args):
     alpha = torch.tensor(1) - torch.exp(-sigma * dist) # alpha[N_rays, N_samples]
     eps = 1e-10
     T = torch.cumprod(torch.cat([torch.tensor(1).expand(alpha.shape[0],1), 1.-alpha[:,:-1]+eps],dim = -1), dim = 1)
-    pixels = torch.sum(T[...,None] * alpha[...,None] * rgb, dim = -2)
-    return pixels
+    weight = T * alpha
+    pixels = torch.sum(weight[...,None] * rgb, dim = -2)
+    return pixels, weight
 
-def points2pixels(points, model, model_fine, args):
+def points2pixels(rays, points, z_vals, model, model_fine, args):
     points = points.reshape((-1,3+3)) # points[N*H*W*N_samples, (x_dim + dir_dim)]
     x, d = points.split([3,3], dim = -1)
     view_d = d /torch.norm(d, dim = -1, keepdim = True) 
@@ -166,6 +168,8 @@ def points2pixels(points, model, model_fine, args):
     points_encoded = torch.cat([x_encoded, d_encoded], axis = -1)
     # exit()
     pixels_list = []
+    weight_list = []
+    print(points_encoded.shape)
     for i in range(0, points_encoded.shape[0], args.batch_points):
         if i + args.batch_points > points.shape[0]:
             end = points_encoded.shape[0]
@@ -173,11 +177,71 @@ def points2pixels(points, model, model_fine, args):
             end = i + args.batch_points
         raws = model(points_encoded[i:end,:])
         assert(args.batch_points % args.N_samples == 0)
-        pixels = raws2pixels(raws, points[i:end], args)
+        pixels, weight = raws2pixels(raws, points[i:end], args, args.N_samples)
         pixels_list.append(pixels)
-        # print(i, points_encoded.shape[0])
+        weight_list.append(weight)
+
     pixels = torch.cat(pixels_list, dim = 0)
-    return pixels
+    weight = torch.cat(weight_list, dim = 0)
+    pixels_fine = None
+    weight_fine = None
+    
+    if(args.N_importance > 0):
+        weight = weight[...,1:-1] + 1e-5 # 防止weight=0 # 原版实现没有使用weight[...,0], 生成cdf时用0代替
+        weight = weight / torch.sum(weight, -1, keepdim = True)
+        cdf = torch.cumsum(weight, -1)
+        cdf = torch.cat([torch.zeros_like(weight[...,:1]), cdf], axis = -1) # cdf[N*h*w, N_samples-1]
+        
+        y_uniform = torch.linspace(0, 1, args.N_importance).expand(list(cdf.shape[:-1]) + [args.N_importance])
+        y_uniform = y_uniform.contiguous() # 否则下一句会报warning
+        idx = torch.searchsorted(cdf, y_uniform, right = True)
+        below = torch.max(torch.zeros_like(idx), idx - 1)
+        above = torch.min(torch.ones_like(idx)*(cdf.shape[-1]-1), idx)
+        cdf_below = torch.gather(cdf, -1, below)
+        cdf_above = torch.gather(cdf, -1, above)
+        dnorm = cdf_above - cdf_below
+        dnorm = torch.where(dnorm < 1e-5, torch.ones_like(dnorm), dnorm)
+        t = (y_uniform - cdf_below) / dnorm
+
+        z_vals_mid = (z_vals[...,1:] + z_vals[...,:-1]) * 0.5
+        z_vals_below = torch.gather(z_vals_mid.expand(cdf.shape), -1, below)
+        z_vals_above = torch.gather(z_vals_mid.expand(cdf.shape), -1, above)
+        
+        z_vals_fine = z_vals_below + t * (z_vals_above - z_vals_below)
+        z_vals_fine, _ = torch.sort(torch.cat([z_vals.expand(cdf.shape[0], z_vals.shape[0]), z_vals_fine], -1), -1)
+
+        rays_o, rays_d = rays[...,0], rays[...,1]
+
+        points_fine = rays_o[:,None,:] + z_vals_fine[:,:,None] * rays_d[:,None, :] # near,far为各poses的z轴范围, 这里虽然norm(rays_d)不为1,但是rays_d的z值为1, 故直接相乘  (N_rays, N_points, 3)
+        rays_d = rays_d[:,None,:].expand(points_fine.shape)
+        x_fine = points_fine.reshape(-1, 3)
+        rays_d = rays_d.reshape(-1, 3)
+
+        points_fine = torch.cat((x_fine, rays_d), axis = -1)
+        
+        view_d = rays_d /torch.norm(rays_d, dim = -1, keepdim = True) 
+        # 这里论文要求输入模型的view_dir为unit vector. norm之前不是unit vector 而是z值为1的向量
+        x_fine_encoded , d_fine_encoded = encode(x_fine, args.encode_x), encode(view_d, args.encode_d)
+        points_fine_encoded = torch.cat([x_fine_encoded, d_fine_encoded], axis = -1)
+        print(points_fine_encoded.shape)
+        pixels_fine_list = []
+        weight_fine_list = []
+        for i in range(0, points_fine_encoded.shape[0], args.batch_points):
+            if i + args.batch_points > points_fine_encoded.shape[0]:
+                end = points_fine_encoded.shape[0]
+            else:
+                end = i + args.batch_points
+            raws_fine = model_fine(points_fine_encoded[i:end,:])
+            assert(args.batch_points % args.N_samples == 0)
+            pixels_temp, weight_temp = raws2pixels(raws_fine, points_fine[i:end], args, args.N_samples + args.N_importance)
+            pixels_fine_list.append(pixels_temp)
+            weight_fine_list.append(weight_temp)
+
+        pixels_fine = torch.cat(pixels_fine_list, dim = 0)
+        weight_fine = torch.cat(weight_fine_list, dim = 0)
+
+
+    return pixels, pixels_fine
 
 def batch_rays(rays, bds, model, model_fine, args):
     if args.shared_near_far == True:
@@ -185,32 +249,40 @@ def batch_rays(rays, bds, model, model_fine, args):
         far = bds.max()*1.0
 
         pixels_list = []
+        pixels_fine_list = []
         for i in range(0, rays.shape[0], args.batch_rays):
             print(i)
             if i + args.batch_rays > rays.shape[0]:
                 end = rays.shape[0]
             else:
                 end = i + args.batch_rays
-            batch_points = rays2points(rays[i:end,...], near, far, bds, args) # batch_points[N*H*W,N_sample,6] (x+dir)
-            pixels = points2pixels(batch_points, model, model_fine, args)
+            batch_points, z_vals = rays2points(rays[i:end,...], near, far, bds, args) # batch_points[N*H*W,N_sample,6] (x+dir)
+            pixels, pixels_fine = points2pixels(rays[i:end,...], batch_points, z_vals, model, model_fine, args) # rays用于生成important points
             pixels_list.append(pixels)
+            if(args.N_importance > 0):
+                pixels_fine_list.append(pixels_fine)
             print("rays", i/196608., rays.shape[0])
 
         pixels = torch.cat(pixels_list, dim = 0)
-        print(len(pixels_list))
-        print(pixels.shape)
+        if(args.N_importance > 0):
+            pixels_fine = torch.cat(pixels_fine_list, dim = 0)
+
+        print(pixels_fine.shape)
     else:
         pass
-    return pixels
+    return pixels, pixels_fine
 
 def render_poses_fn(render_poses, bds, model, model_fine, args): # render_poses[N,3,5], bds[N,2]
     hwf = render_poses[0,:,-1]
     H, W, f = int(hwf[0]), int(hwf[1]), hwf[2]
-    rays = poses2rays(render_poses, args) # rays[N*H*W,3,2] (rays_o+rays_d)
-    pixels = batch_rays(rays, bds, model, model_fine, args) # 
-    print(pixels.shape)
+    rays = poses2rays(render_poses, args) # rays[N*H*W,3,2] (rays_o+rays_d) / numpy2torch
+    pixels, pixels_fine = batch_rays(rays, bds, model, model_fine, args) #
     imgs = pixels.reshape((render_poses.shape[0],H,W,3))
-    return imgs
+
+    imgs_fine = None
+    if(args.N_importance > 0):
+        imgs_fine = pixels_fine.reshape((render_poses.shape[0],H,W,3))
+    return imgs, imgs_fine
 
 def train(train_poses, train_images, bds, model, model_fine, optimizer, args):
     rays = poses2rays(train_poses, args) # rays[N*H*W,3,2] (rays_o+rays_d)
@@ -268,14 +340,19 @@ if __name__ =='__main__':
         with torch.no_grad():
             render_images = [f/255 for f in render_images]
             # for i in range(render_poses.shape[0]):
-            render_poses = render_poses[::20]
-            render_images = render_images[::20]
-            render_imgs = render_poses_fn(render_poses, bds, model, model_fine, args)
+            render_poses = render_poses[0:3]
+            render_images = render_images[0:3]
+            render_imgs, render_images_fine = render_poses_fn(render_poses, bds, model, model_fine, args)
             rgb8 = (255 * (render_imgs).cpu().numpy()).astype(np.uint8)
+            rgb8_fine = (255 * (render_images_fine).cpu().numpy()).astype(np.uint8)
             for i in range(render_imgs.shape[0]):
                 imageio.imwrite("./img_" +  str(i) + ".png", rgb8[i])
+                imageio.imwrite("./img_fine_" +  str(i) + ".png", rgb8_fine[i])
                 imageio.imwrite("./img_GT_" + str(i) + ".png", render_images[i])
                 mse = torch.mean((render_imgs[i]-torch.tensor(render_images[i]).to(device))**2)
+                psnr = -10. * torch.log(mse) / torch.log(torch.tensor([10.]))
+                print(i, psnr)
+                mse = torch.mean((render_images_fine[i]-torch.tensor(render_images[i]).to(device))**2)
                 psnr = -10. * torch.log(mse) / torch.log(torch.tensor([10.]))
                 print(i, psnr)
     else:
